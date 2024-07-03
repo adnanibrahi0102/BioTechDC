6e31900e167adf30c2d7665b074da468b9ef61ec
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-07-03 15:14:48 +0530

    testing

--- src/config/axiosInstance.js
+++ src/config/axiosInstance.js
@@ -8,7 +8,10 @@ const axiosInstance = axios.create({
 // Add a request interceptor to include authorization token
 axiosInstance.interceptors.request.use(
     (config) => {
+        console.log('localStorage:', localStorage);
         const accessToken = localStorage.getItem('accessToken');
+        console.log('accessToken:', accessToken);
+
         console.log('accessToken:', accessToken); // Log accessToken for debugging
         if (accessToken) {
             config.headers.Authorization = `Bearer ${accessToken}`;
