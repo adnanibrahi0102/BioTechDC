f592bf7f8d2a11eb4684a2559b2573f3017dd961
Adnan ibrahim <adnanibrahi96@gmail.com>
2024-07-05 18:40:55 +0530

    updated

--- src/components/AllPatients.jsx
+++ src/components/AllPatients.jsx
@@ -94,7 +94,7 @@ const AllPatients = () => {
         />
       </div>
       {loading ? (
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center h-screen">
           <div
             className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full"
             role="status"
